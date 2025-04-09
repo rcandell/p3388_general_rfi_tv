2dcaed91be156d12fa9bb0de64678e7c35a78e75
Rick Candell <rick.candell@gmail.com>
2025-04-09 09:48:46 -0400

    updated jspec

--- makejspec.m
+++ makejspec.m
@@ -3,7 +3,8 @@ clear all;
 fclose all;
 close all;
 
+rng("shuffle")
 config_file = "./config/jspec_config_template.json";
-csv_file = "./config/jspecs - carrier.xlsx";
+csv_file = "./config/jspecs - all.xlsx";
 output_file = "./config/reactors_autogen.json";
 rfigen.jspec_csvtojson(config_file, csv_file, output_file);
