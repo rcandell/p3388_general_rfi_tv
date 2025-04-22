28f7a5b26ad837ea9eae215c113d90bc3a7ed836
Rick Candell <rick dot candell at gmail dot com>
2025-04-22 15:06:11 -0400

    change to 4 carriers only

--- makejspec.m
+++ makejspec.m
@@ -5,6 +5,6 @@ close all;
 
 rng("shuffle")
 config_file = "./config/jspec_config_template.json";
-csv_file = "./config/jspecs - all.xlsx";
+csv_file = "./config/jspecs - carrier.xlsx";
 output_file = "./config/reactors_autogen.json";
 rfigen.jspec_csvtojson(config_file, csv_file, output_file);
